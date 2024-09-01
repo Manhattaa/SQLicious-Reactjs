3bbf0a1ed9eb1b734840dce0abae2d7cc749f0e3
manhattaa <fadynhatta@gmail.com>
2024-09-02 01:40:33 +0200

    Created a BookingController that will handle the logic with the customer booking side

--- SQLicious.Server/Controllers/BookingController.cs
+++ SQLicious.Server/Controllers/BookingController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SQLicious.Server.Model.DTOs;
+using SQLicious.Server.Services.IServices;
 
 namespace SQLicious.Server.Controllers
 {
@@ -7,5 +9,46 @@ namespace SQLicious.Server.Controllers
     [ApiController]
     public class BookingController : ControllerBase
     {
+        private readonly IBookingService _bookingService;
+
+        public BookingController(IBookingService bookingService)
+        {
+            _bookingService = bookingService;
+        }
+
+        [HttpGet("{bookingId}")]
+        public async Task<ActionResult<CustomerDTO>> GetBookingById(int bookingId)
+        {
+            var booking = await _bookingService.GetBookingByIdAsync(bookingId);
+            return Ok(booking);
+        }
+
+        [HttpPost("Reserve")]
+        public async Task<ActionResult> CreateBooking(BookingCreationDTO booking)
+        {
+            await _bookingService.ReserveATableAsync(booking);
+            return Ok("Booking created successfully.");
+        }
+
+        [HttpPut("Update")]
+        public async Task<ActionResult> UpdateBooking(BookingDTO booking)
+        {
+            await _bookingService.UpdateBookingAsync(booking);
+            return Ok("Booking updated successfully.");
+        }
+
+        [HttpDelete("Delete")]
+        public async Task<ActionResult> DeleteBooking(int bookingId)
+        {
+            await _bookingService.DeleteBookingAsync(bookingId);
+            return Ok("Booking deleted successfully.");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BookingDTO>>> GetAllBookings()
+        {
+            var bookingList = await _bookingService.GetAllBookingsAsync();
+            return Ok(bookingList);
+        }
     }
 }
