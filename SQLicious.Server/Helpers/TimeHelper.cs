61c109a543d91dccf376d571aecedb037f7e8f57
manhattaa <fadynhatta@gmail.com>
2024-09-02 00:25:28 +0200

    Created a TimeHelper that will reset time so we get rid of unnecessary seconds

---
+++ SQLicious.Server/Helpers/TimeHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace SQLicious.Server.Helpers
+{
+    public class TimeHelper
+    {
+        public static DateTime DateTimeHelper(DateTime dateTime)
+        {
+            return dateTime.Date.AddHours(dateTime.Hour).AddMinutes(dateTime.Minute);
+        }
+    }
+}
