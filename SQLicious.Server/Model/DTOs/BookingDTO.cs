fcfa981f3f35cdad1b7e2ba182ccd39edaea8344
manhattaa <fadynhatta@gmail.com>
2024-09-02 00:31:28 +0200

    Spelling error!

--- SQLicious.Server/Model/DTOs/BookingDTO.cs
+++ SQLicious.Server/Model/DTOs/BookingDTO.cs
@@ -5,7 +5,7 @@ namespace SQLicious.Server.Model.DTOs
     public class BookingDTO
     {
         public int BookingId { get; set; }
-        public int ÁmountOfCustomers { get; set; }
+        public int AmountOfCustomers { get; set; }
         public DateTime BookedDateTime { get; set; }
         public int UserId { get; set; }
         public int TableId { get; set; }
