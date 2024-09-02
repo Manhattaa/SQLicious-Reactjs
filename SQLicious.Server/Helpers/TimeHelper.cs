faf69a17fed34b075a95555b3a8429ada3267b49
manhattaa <fadynhatta@gmail.com>
2024-09-02 02:02:25 +0200

    Created a TimeHelper that decreases clutter and removes the Seconds in DateTime will be especially useful in the frontend

--- SQLicious.Server/Helpers/TimeHelper.cs
+++ SQLicious.Server/Helpers/TimeHelper.cs
@@ -2,6 +2,7 @@
 {
     public class TimeHelper
     {
+        //Handles and resets time so we wont have to worry about the clutter
         public static DateTime DateTimeHelper(DateTime dateTime)
         {
             return dateTime.Date.AddHours(dateTime.Hour).AddMinutes(dateTime.Minute);
