86f206773277d67180b9e9c9aa581bd5a1a76c43
manhattaa <fadynhatta@gmail.com>
2024-09-02 02:03:19 +0200

    created a CustomerController that will handle the Customer related queries.

---
+++ SQLicious.Server/Controllers/CustomerController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SQLicious.Server.Model.DTOs;
+using SQLicious.Server.Services.IServices;
+
+namespace SQLicious.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpGet("{customerId}")]
+        public async Task<ActionResult<CustomerDTO>> GetCustomerById(int customerId)
+        {
+            var customer = await _customerService.GetCustomerByIdAsync(customerId);
+            return Ok(customer);
+        }
+
+        [HttpPost("Create")]
+        public async Task<ActionResult> CreateCustomer(CustomerCreationDTO customer)
+        {
+            await _customerService.CreateCustomerAsync(customer);
+            return Ok("Customer created successfully.");
+        }
+
+        [HttpPut("Update")]
+        public async Task<ActionResult> UpdateCustomer(CustomerDTO customer)
+        {
+            await _customerService.UpdateCustomerAsync(customer);
+            return Ok("Customer updated successfully.");
+        }
+
+        [HttpDelete("Delete")]
+        public async Task<ActionResult> DeleteCustomer(int customerId)
+        {
+            await _customerService.DeleteCustomerAsync(customerId);
+            return Ok("Customer deleted successfully.");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CustomerDTO>>> GetAllCustomers()
+        {
+            var customersList = await _customerService.GetAllCustomersAsync();
+            return Ok(customersList);
+        }
+    }
+}
