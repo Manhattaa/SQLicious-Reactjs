3b3052fb9efa846a7c4a023e8044d193e2ff80c9
Fady <145761517+Manhattaa@users.noreply.github.com>
2024-09-16 16:13:42 +0200

    Added pathing to ContactPage and ConfirmationPage

--- sqlicious.client/src/App.jsx
+++ sqlicious.client/src/App.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import IndexPage from './pages/IndexPage';
 import BookingPage from './pages/BookingPage';
 import Header from './components/Header';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import ConfirmationPage from './pages/ConfirmationPage';
 
 function App() {
     
@@ -12,6 +14,8 @@ function App() {
       <Routes>
         <Route path="/" element={ <><Header /> <IndexPage /> </>} />
         <Route path="/booking" element={<><Header /> <BookingPage /> </>} />
+        <Route path="/contact" element={<><Header /> <ContactPage /> </>} />
+        <Route path="/confirm" element={<><Header /> <ConfirmationPage /> </>} />
       </Routes>
     </BrowserRouter>
             
