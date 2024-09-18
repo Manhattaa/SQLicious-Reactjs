1dbe954f57077a871017a5a3ac1e97a7111e043b
manhattaa <fadynhatta@gmail.com>
2024-09-19 00:34:23 +0200

    Created a DeleteAdminRequestModel that will handle the Deletion requests of Admin

---
+++ SQLicious.Server/Model/DTOs/Admin/DeleteAdminRequestModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SQLicious.Server.Model.DTOs.Admin
+{
+    public class DeleteAdminRequestModel
+    {
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        [StringLength(256, MinimumLength = 8)]
+        public string Password { get; set; }
+    }
+}
