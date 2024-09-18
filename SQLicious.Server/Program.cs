6cabbeda815184fad27366cb9df401c04a515e47
manhattaa <fadynhatta@gmail.com>
2024-09-19 00:44:54 +0200

    Added all Scoped all Repositories and Services into Program.cs as well as implemented the EmailServer and Added CORS

--- SQLicious.Server/Program.cs
+++ SQLicious.Server/Program.cs
@@ -6,6 +6,16 @@ using SQLicious.Server.Data;
 using DotNetEnv;
 using SQLicious.Server.Services.IServices;
 using SQLicious.Server.Services;
+using Microsoft.AspNetCore.Cors;
+using SQLicious.Server.Options;
+using Microsoft.AspNetCore.Identity;
+using SQLicious.Server.Model;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using SQLicious.Server.Options.Email;
+using SQLicious.Server.Options.Email.IEmail;
 
 namespace SQLicious.Server
 {
@@ -17,6 +27,46 @@ namespace SQLicious.Server
 
             Env.Load();
 
+            var mailKitSettingsSection = new MailKitSettings
+            {
+                MailServer = Environment.GetEnvironmentVariable("MAILSERVER"),
+                MailPort = int.Parse(Environment.GetEnvironmentVariable("MAILPORT")),
+                SenderName = Environment.GetEnvironmentVariable("SENDERNAME"),
+                Sender = Environment.GetEnvironmentVariable("SENDER"),
+                Password = Environment.GetEnvironmentVariable("PASSWORD")
+            };
+
+            builder.Services.Configure<MailKitSettings>(options =>
+            {
+                options.MailServer = mailKitSettingsSection.MailServer;
+                options.MailPort = mailKitSettingsSection.MailPort;
+                options.SenderName = mailKitSettingsSection.SenderName;
+                options.Sender = mailKitSettingsSection.Sender;
+                options.Password = mailKitSettingsSection.Password;
+            });
+
+            // Identity Setup
+            builder.Services.AddIdentity<Admin, IdentityRole>()
+                .AddEntityFrameworkStores<RestaurantContext>()
+                .AddDefaultTokenProviders();
+
+            // Adding JWT Bearer Authentication
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.SaveToken = true;
+                    options.RequireHttpsMetadata = true;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidIssuer = Environment.GetEnvironmentVariable("JWT_ISSUER"),
+                        ValidAudience = Environment.GetEnvironmentVariable("JWT_AUDIENCE"),
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("JWT_SECRET")))
+                    };
+                });
+
             // Add services to the container.
             builder.Services.AddDbContext<RestaurantContext>(options =>
             {
@@ -24,18 +74,80 @@ namespace SQLicious.Server
             });
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+            //CORS
+            builder.Services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.WithOrigins("https://localhost:5173", "https://localhost:7213", "https://localhost:7294")
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials();
+                });
+            });
+         
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            //Table
             builder.Services.AddScoped<ITableRepository, TableRepository>();
             builder.Services.AddScoped<ITableService, TableService>();
-
+            //Booking
             builder.Services.AddScoped<IBookingRepository, BookingRepository>();
             builder.Services.AddScoped<IBookingService, BookingService>();
-
+            //Customer
             builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
             builder.Services.AddScoped<ICustomerService, CustomerService>();
+            //Admin
+            builder.Services.AddScoped<IAdminRepository, AdminRepository>();
+            builder.Services.AddScoped<IAdminService, AdminService>();
+            //MenuItem
+            builder.Services.AddScoped<IMenuItemRepository, MenuItemRepository>();
+            builder.Services.AddScoped<IMenuItemService, MenuItemService>();
+            //Jwt & Authentication
+            builder.Services.AddScoped<JwtRepository>();
+            builder.Services.AddScoped<AuthenticationService>();
+            //Email
+            builder.Services.AddScoped<IEmailSender, EmailSender>();
+
+
+            //Add Authorization
+            builder.Services.AddAuthorization();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SQLicious API", Version = "v1" });
+
+                // Define Bearer Authentication scheme for Swagger
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer"
+                });
+
+                // Apply Bearer authentication globally in Swagger UI
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        },
+                        Scheme = "oauth2",
+                        Name = "Bearer",
+                        In = ParameterLocation.Header,
+                    },
+                    new List<string>()
+                }
+            });
+            });
 
             var app = builder.Build();
 
@@ -53,6 +165,7 @@ namespace SQLicious.Server
 
             app.UseAuthorization();
 
+            app.UseCors();
 
             app.MapControllers();
 
