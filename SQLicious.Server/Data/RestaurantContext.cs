5da18e9453d9d945acc0a5a69a3407f9d3adf538
manhattaa <fadynhatta@gmail.com>
2024-09-19 00:32:22 +0200

    Connected Identity to the Admin Model

--- SQLicious.Server/Data/RestaurantContext.cs
+++ SQLicious.Server/Data/RestaurantContext.cs
@@ -1,17 +1,18 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using SQLicious.Server.Model;
 using System.Collections.Generic;
 
 namespace SQLicious.Server.Data
 {
-    public class RestaurantContext : DbContext
+    public class RestaurantContext : IdentityDbContext<Admin>
     {
         public RestaurantContext(DbContextOptions<RestaurantContext> options) : base(options) { }
         public DbSet<Table> Tables { get; set; }
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Booking> Bookings { get; set; }
         public DbSet<Admin> Admins { get; set; }
-        public DbSet<MenuItems> Menus { get; set; }
+        public DbSet<MenuItems> MenuItems { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
