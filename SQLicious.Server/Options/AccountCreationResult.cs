e2e84b1f9d264d0a213eec238212192e51a9e966
manhattaa <fadynhatta@gmail.com>
2024-09-19 00:42:07 +0200

    Created an AccountCreationResult and LoginResult that will help us get results

---
+++ SQLicious.Server/Options/AccountCreationResult.cs
@@ -0,0 +1,10 @@
+﻿namespace SQLicious.Server.Options
+{
+    public class AccountCreationResult
+    {
+        public bool Success { get; set; }
+        public string? Token { get; set; }
+        //To support multiple errorMessages
+        public IEnumerable<string>? Errors { get; set; }
+    }
+}
