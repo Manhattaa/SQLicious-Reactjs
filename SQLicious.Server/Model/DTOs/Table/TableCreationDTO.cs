1985a28be8bdc5e704f4c31bfb0c3601284d7e07
manhattaa <fadynhatta@gmail.com>
2024-09-19 00:38:48 +0200

    Added a TableCreationDTO and TableDTO that will help us with POST and GET requests

---
+++ SQLicious.Server/Model/DTOs/Table/TableCreationDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace SQLicious.Server.Model.DTOs.Table
+{
+    public class TableCreationDTO
+    {
+        public int Capacity { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
---
+++ SQLicious.Server/Model/DTOs/Table/TableDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace SQLicious.Server.Model.DTOs.Table
+{
+    public class TableDTO
+    {
+        public int TableId { get; set; }
+        public int SeatingCapacity { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
