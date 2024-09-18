dba0e18496cf2e5a96d70a222555b4235cad1187
manhattaa <fadynhatta@gmail.com>
2024-09-19 00:33:40 +0200

    Created a ConfirmEmailRequestDTO

---
+++ SQLicious.Server/Model/DTOs/Admin/ConfirmEmailRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace SQLicious.Server.Model.DTOs.Admin
+{
+    public class ConfirmEmailRequestDTO
+    {
+        public string UserId { get; set; }
+        public string Code { get; set; }
+    }
+}
