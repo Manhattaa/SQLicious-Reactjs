77bf56946355674689d686004d7787a7beb04b97
manhattaa <fadynhatta@gmail.com>
2024-09-19 00:35:23 +0200

    Created a BookingCreationDTO that will scope the Booking Related queries.

---
+++ SQLicious.Server/Model/DTOs/Booking/BookingCreationDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SQLicious.Server.Model.DTOs.Booking
+{
+    public class BookingCreationDTO
+    {
+        public int AmountOfCustomers { get; set; }
+        public DateTime BookedDateTime { get; set; }
+        public int CustomerId { get; set; }
+        public int TableId { get; set; }
+    }
+}
