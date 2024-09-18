d3c534aecce78c0c6e8eb09c3a0a760cd1c2d3b8
manhattaa <fadynhatta@gmail.com>
2024-09-19 00:36:29 +0200

    Created a BookingDTO that will be injected into certain GET requests for the Booking services

---
+++ SQLicious.Server/Model/DTOs/Booking/BookingDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SQLicious.Server.Model.DTOs.Booking
+{
+    public class BookingDTO
+    {
+        public int BookingId { get; set; }
+        public int AmountOfCustomers { get; set; }
+        public int CustomerId { get; set; }
+        public int TableId { get; set; }
+        public DateTime BookedDateTime { get; set; }
+    }
+}
