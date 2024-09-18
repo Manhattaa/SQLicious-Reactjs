c3627c9a38e1463bac82c662f560ccf79ef072d7
manhattaa <fadynhatta@gmail.com>
2024-09-19 00:31:39 +0200

    Added a MenuItem Repository layer

---
+++ SQLicious.Server/Data/Repository/IRepositories/IMenuItemRepository.cs
@@ -0,0 +1,13 @@
+﻿using SQLicious.Server.Model;
+
+namespace SQLicious.Server.Data.Repository.IRepositories
+{
+    public interface IMenuItemRepository
+    {
+        Task<IEnumerable<MenuItems>> GetAllMenuItemsAsync();
+        Task<MenuItems> GetMenuItemByIdAsync(int menuItemId);
+        Task CreateMenuItemAsync(MenuItems menuItem);
+        Task UpdateMenuItemAsync(MenuItems menuItem);
+        Task DeleteMenuItemAsync(int menuItemId);
+    }
+}
