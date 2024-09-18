b8a0101a8edc3847c0fb3526e442805b40d72877
manhattaa <fadynhatta@gmail.com>
2024-09-19 00:37:05 +0200

    Added a CustomerCreationDTO that will aid us when creating Customers.

---
+++ SQLicious.Server/Model/DTOs/Customer/CustomerCreationDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace SQLicious.Server.Model.DTOs.Customer
+{
+    public class CustomerCreationDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
