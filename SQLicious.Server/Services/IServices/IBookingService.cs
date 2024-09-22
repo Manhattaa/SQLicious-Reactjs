e3df84cd94242a3f51f99599086ec541cac6d44b
manhattaa <fadynhatta@gmail.com>
2024-09-22 18:37:56 +0200

    Updated ReserveATableAsync to use IdentityResult

--- SQLicious.Server/Services/IServices/IBookingService.cs
+++ SQLicious.Server/Services/IServices/IBookingService.cs
@@ -1,12 +1,15 @@
-﻿using SQLicious.Server.Model;
+﻿using Microsoft.AspNetCore.Identity;
+using SQLicious.Server.Model;
 using SQLicious.Server.Model.DTOs.Booking;
+using SQLicious.Server.Model.DTOs.Customer;
+using SQLicious.Server.Model.DTOs.MenuItem;
 
 namespace SQLicious.Server.Services.IServices
 {
     public interface IBookingService
     {
         Task<IEnumerable<BookingDTO>> GetAllBookingsAsync();
-        Task ReserveATableAsync(BookingCreationDTO booking);
+        Task <IdentityResult>ReserveATableAsync(BookingCreationDTO booking, CustomerCreationDTO customerDetails, MenuItemDTO menuItemDto);
         Task<BookingDTO> GetBookingByIdAsync(int bookingId);
         Task CustomerTableExistsAsync(int tableId, int customerId);
         Task UpdateBookingAsync(BookingDTO booking);
