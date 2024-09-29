7fcef6aa0884ebbc27a2e78e38302b6e31a532a2
manhattaa <fadynhatta@gmail.com>
2024-09-29 22:47:38 +0200

    Added AuthenticatorKey as a parameter in the Admin Model

--- SQLicious.Server/Model/Admin.cs
+++ SQLicious.Server/Model/Admin.cs
@@ -5,5 +5,7 @@ namespace SQLicious.Server.Model
     {
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
+
+        public string? AuthenticatorKey { get; set; }
     }
 }
