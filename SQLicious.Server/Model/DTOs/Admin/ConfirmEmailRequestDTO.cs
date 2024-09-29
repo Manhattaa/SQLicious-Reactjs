0774eff5186aea448b62abb997c126946c3c5ea2
manhattaa <fadynhatta@gmail.com>
2024-09-29 22:47:11 +0200

    Fixed a typo in the ConfirmEmailRequestDTO

--- SQLicious.Server/Model/DTOs/Admin/ConfirmEmailRequestDTO.cs
+++ SQLicious.Server/Model/DTOs/Admin/ConfirmEmailRequestDTO.cs
@@ -2,7 +2,7 @@
 {
     public class ConfirmEmailRequestDTO
     {
-        public string UserId { get; set; }
+        public string Id { get; set; }
         public string Code { get; set; }
     }
 }
